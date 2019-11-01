261c88b6db49293fe9172776cc707a26b0d8c305
paul.montagna <paul.montagna@gmail.com>
2019-11-01 15:12:28 +1100

    initial commit

---
+++ About.aspx.vb
@@ -0,0 +1,13 @@
+﻿Imports React
+
+Public Class About
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Dim env = AssemblyRegistration.Container.Resolve(Of IReactEnvironment)()
+        Dim objectModel = New With {Key .user = "React User"}
+        Dim reactComponent = env.CreateComponent("ReactContent", objectModel)
+
+        PageContent.Text = reactComponent.RenderHtml()
+    End Sub
+End Class
---
+++ App_Start/ReactConfig.vb
@@ -0,0 +1,30 @@
+Imports React
+Imports JavaScriptEngineSwitcher.Core
+Imports JavaScriptEngineSwitcher.V8
+
+<Assembly: WebActivatorEx.PreApplicationStartMethod(GetType(ReactConfig), "Configure")>
+
+Public Module ReactConfig
+    Sub Configure()
+
+        JsEngineSwitcher.Current.DefaultEngineName = V8JsEngine.EngineName
+        JsEngineSwitcher.Current.EngineFactories.AddV8()
+        '// If you want to use server-side rendering of React components, 
+        '// add all the necessary JavaScript files here. This includes 
+        '// your components as well as all of their dependencies.
+        '// See http://reactjs.net/ for more information. Example
+        ReactSiteConfiguration.Configuration = New ReactSiteConfiguration().AddScript("~/Scripts/React/index.jsx")
+        '//	.AddScript("~/Scripts/First.jsx")
+        '//	.AddScript("~/Scripts/Second.jsx");
+
+        '// If you use an external build too (for example, Babel, Webpack,
+        '// Browserify Or Gulp), you can improve performance by disabling 
+        '// ReactJS.NET's version of Babel and loading the pre-transpiled 
+        '// scripts. Example:
+        '//ReactSiteConfiguration.Configuration
+        '//	.SetLoadBabel(false)
+        '//	.AddScriptWithoutTransform("~/Scripts/bundle.server.js")
+    End Sub
+
+End Module
+
